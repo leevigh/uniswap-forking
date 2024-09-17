e1ac56da3f42c9df1c38ef1c8ab2294057781887
Levi Francis <levifrancis15@gmail.com>
2024-09-17 01:00:02 +0100

    interact with removeLiquidity function

--- contracts/interfaces/IUniswapV2Router.sol
+++ contracts/interfaces/IUniswapV2Router.sol
@@ -41,7 +41,7 @@ interface IUniswapV2Router {
         uint amountETHMin,
         address to,
         uint deadline
-      ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
 
     
     function removeLiquidity(
@@ -52,18 +52,18 @@ interface IUniswapV2Router {
         uint amountBMin,
         address to,
         uint deadline
-      ) external returns (uint amountA, uint amountB);
+    ) external returns (uint amountA, uint amountB);
 
-      function removeLiquidityETH(
+    function removeLiquidityETH(
         address token,
         uint liquidity,
         uint amountTokenMin,
         uint amountETHMin,
         address to,
         uint deadline
-      ) external returns (uint amountToken, uint amountETH);
+    ) external returns (uint amountToken, uint amountETH);
 
-      function removeLiquidityETHWithPermit(
+    function removeLiquidityETHWithPermit(
         address token,
         uint liquidity,
         uint amountTokenMin,
@@ -71,5 +71,5 @@ interface IUniswapV2Router {
         address to,
         uint deadline,
         bool approveMax, uint8 v, bytes32 r, bytes32 s
-      ) external returns (uint amountToken, uint amountETH);
+    ) external returns (uint amountToken, uint amountETH);
 }
--- scripts/removeLiquidity.ts
+++ scripts/removeLiquidity.ts
@@ -6,12 +6,15 @@ async function main() {
     // Get all addresses of tokens and users to interact with
     const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
     const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    const USDC_DAI_PAIR = "0xAE461cA67B15dc8dc81CE7615e0320dA1A9aB8D5";
     const UniSwapRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-    const USDCHolder = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621";
 
-    // impersonate holder and make holder a signer
-    await helpers.impersonateAccount(USDCHolder);
-    const impersonatedSigner = await ethers.getSigner(USDCHolder);
+    const TokenHolder = "0xf584f8728b874a6a5c7a8d4d387c9aae9172d621";
+
+    await helpers.impersonateAccount(TokenHolder);
+
+    // impersonated address a signer
+    const impersonatedSigner = await ethers.getSigner(TokenHolder);
 
     // set parameter values
     const amountUSDCDesired = ethers.parseUnits("1000", 6);
@@ -25,6 +28,7 @@ async function main() {
     // get token contract snapshots for the impersonated user to approve spending
     const USDC = await ethers.getContractAt("IERC20", USDCAddress, impersonatedSigner);
     const DAI = await ethers.getContractAt("IERC20", DAIAddress, impersonatedSigner);
+    const LP_Contract = await ethers.getContractAt("IERC20", USDC_DAI_PAIR, impersonatedSigner);
     const ROUTER = await ethers.getContractAt("IUniswapV2Router", UniSwapRouter, impersonatedSigner);
 
     // approve spending on tokens
@@ -34,8 +38,10 @@ async function main() {
     // check balance before swap
     const usdcBefore = await USDC.balanceOf(impersonatedSigner.getAddress());
     const daiBefore = await DAI.balanceOf(impersonatedSigner.getAddress());
+    const lpBalBefore = await LP_Contract.balanceOf(impersonatedSigner.getAddress());
 
     console.log("Balance before adding liquidity:::", "USDC:::", Number(usdcBefore), "DAI:::", Number(daiBefore));
+    console.log("Balance of LP before adding Liquidity:::", Number(lpBalBefore));
 
     // Perform addLiquidity transaction
     const tx = await ROUTER.addLiquidity(
@@ -54,11 +60,42 @@ async function main() {
     // check balance before swap
     const usdcAfter = await USDC.balanceOf(impersonatedSigner.getAddress());
     const daiAfter = await DAI.balanceOf(impersonatedSigner.getAddress());
+    const lpBalAfter = await LP_Contract.balanceOf(impersonatedSigner.getAddress());
 
     console.log("Balance after adding liquidity:::", "USDC:::", Number(usdcAfter), "DAI:::", Number(daiAfter));
+    console.log("Balance of LP after adding Liquidity:::", Number(lpBalAfter))
+
+    // Remove Liquidity
+
+    // approve v2 router to spend lp tokens
+    await LP_Contract.approve(UniSwapRouter, lpBalAfter)
+
+    const removeLiqtx = await ROUTER.removeLiquidity(
+        USDC,
+        DAI,
+        lpBalAfter,
+        0,
+        0,
+        impersonatedSigner.getAddress(),
+        deadline
+    )
+
+    removeLiqtx.wait();
+
+    const lpBalAfterRemoval = await LP_Contract.balanceOf(impersonatedSigner.getAddress());
+
+    console.log("LP Balance After Removal:::", lpBalAfterRemoval);
+
+    // Check final token balances
+    const finalUsdcBal = await USDC.balanceOf(impersonatedSigner.address);
+    const finalDaiBal = await DAI.balanceOf(impersonatedSigner.address);
+
+    console.log("Final USDC balance:", Number(finalUsdcBal));
+    console.log("Final DAI balance:", Number(finalDaiBal));
+
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
